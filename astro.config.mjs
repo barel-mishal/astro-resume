879fab977cd42ab06c001182591f1339c39ed90e
barel-mishal <barel.mishal@mail.huji.ac.il>
2022-12-23 19:01:17 +0200

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   vite: {
     ssr: {
-      external: ['svgo'],
-    },
+      external: ['svgo']
+    }
   },
+  output: "server",
+  adapter: netlify()
 });
